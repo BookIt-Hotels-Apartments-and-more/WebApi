6a9bab4aeffc9db12de7898cb9e82b7609756fb1
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2025-07-05 22:58:16 +0300

    Fix

--- BookIt.API/BookIt.API/Mapping/MappingProfiles/EstablishmentsMappingProfile.cs
+++ BookIt.API/BookIt.API/Mapping/MappingProfiles/EstablishmentsMappingProfile.cs
@@ -18,7 +18,8 @@ public class EstablishmentsMappingProfile : Profile
             .ForMember(e => e.CreatedAt, o => o.Ignore());
 
         CreateMap<Establishment, EstablishmentDTO>()
-            .ForMember(dto => dto.Photos, o => o.MapFrom(e => e.Photos.Select(im => im.BlobUrl)));
+            .ForMember(dto => dto.Photos, o => o.MapFrom(e => e.Photos.Select(im => new ImageDTO { BlobUrl = im.BlobUrl })))
+            .ForMember(dto => dto.Owner, o => o.MapFrom(e => e.Owner));
 
         CreateMap<EstablishmentDTO, EstablishmentResponse>();
 
@@ -28,19 +29,19 @@ public class EstablishmentsMappingProfile : Profile
         CreateMap<OwnerDTO, OwnerResponse>();
 
         CreateMap<EstablishmentDTO, EstablishmentResponse>()
-        .ForMember(dest => dest.Features, opt => opt.MapFrom(src => new EstablishmentFeaturesResponse
-        {
-            Parking = (src.Features & EstablishmentFeatures.Parking) != 0,
-            Pool = (src.Features & EstablishmentFeatures.Pool) != 0,
-            Beach = (src.Features & EstablishmentFeatures.Beach) != 0,
-            Fishing = (src.Features & EstablishmentFeatures.Fishing) != 0,
-            Sauna = (src.Features & EstablishmentFeatures.Sauna) != 0,
-            Restaurant = (src.Features & EstablishmentFeatures.Restaurant) != 0,
-            Smoking = (src.Features & EstablishmentFeatures.Smoking) != 0,
-            AccessibleForDisabled = (src.Features & EstablishmentFeatures.AccessibleForDisabled) != 0,
-            ElectricCarCharging = (src.Features & EstablishmentFeatures.ElectricCarCharging) != 0,
-            Elevator = (src.Features & EstablishmentFeatures.Elevator) != 0
-        }))
-        .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));
+            .ForMember(dest => dest.Features, opt => opt.MapFrom(src => new EstablishmentFeaturesResponse
+            {
+                Parking = (src.Features & EstablishmentFeatures.Parking) != 0,
+                Pool = (src.Features & EstablishmentFeatures.Pool) != 0,
+                Beach = (src.Features & EstablishmentFeatures.Beach) != 0,
+                Fishing = (src.Features & EstablishmentFeatures.Fishing) != 0,
+                Sauna = (src.Features & EstablishmentFeatures.Sauna) != 0,
+                Restaurant = (src.Features & EstablishmentFeatures.Restaurant) != 0,
+                Smoking = (src.Features & EstablishmentFeatures.Smoking) != 0,
+                AccessibleForDisabled = (src.Features & EstablishmentFeatures.AccessibleForDisabled) != 0,
+                ElectricCarCharging = (src.Features & EstablishmentFeatures.ElectricCarCharging) != 0,
+                Elevator = (src.Features & EstablishmentFeatures.Elevator) != 0
+            }))
+            .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));
         }
 }
