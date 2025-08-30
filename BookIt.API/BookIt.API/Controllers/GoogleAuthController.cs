0aba65b8d5ec57a61b92e1343ed704edf106eddc
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2025-08-30 20:40:12 +0300

    Update GoogleAuthController.cs

--- BookIt.API/BookIt.API/Controllers/GoogleAuthController.cs
+++ BookIt.API/BookIt.API/Controllers/GoogleAuthController.cs
@@ -28,15 +28,11 @@ public class GoogleAuthController : ControllerBase
     }
 
     [HttpGet("login")]
-    public IActionResult Login([FromQuery] string client)
+    public IActionResult Login()
     {
         try
         {
             var loginUrl = _googleAuthService.GetLoginUrl();
-
-            // Прокидываем client дальше, чтобы callback знал, откуда пришёл запрос
-            loginUrl += $"&state={client}";
-
             return Redirect(loginUrl);
         }
         catch
@@ -47,31 +43,18 @@ public class GoogleAuthController : ControllerBase
     }
 
     [HttpGet("callback")]
-    public async Task<IActionResult> Callback([FromQuery] string code, [FromQuery] string state)
+    public async Task<IActionResult> Callback([FromQuery] string code)
     {
         var clientUrl = _googleOauthSettingsOptions.Value.RedirectClientUri;
 
         try
         {
-            if (string.IsNullOrWhiteSpace(code))
-                return Redirect($"{clientUrl}/auth/error");
-
+            if (string.IsNullOrWhiteSpace(code)) return Redirect($"{clientUrl}/auth/error");
             var (email, name, imageUrl) = await _googleAuthService.GetUserInfoAsync(code);
-            if (string.IsNullOrWhiteSpace(email))
-                return Redirect($"{clientUrl}/auth/error");
-
+            if (string.IsNullOrWhiteSpace(email)) return Redirect($"{clientUrl}/auth/error");
             var user = await _userService.AuthByGoogleAsync(name ?? string.Empty, email, imageUrl);
-            if (user is null)
-                return Redirect($"{clientUrl}/auth/error");
-
+            if (user is null) return Redirect($"{clientUrl}/auth/error");
             var token = await _jwtService.GenerateToken(user);
-
-            // проверяем state (он же client)
-            if (state == "mobile")
-            {
-                return Redirect($"bookit://auth/callback?token={token}");
-            }
-
             return Redirect($"{clientUrl}/auth/success?token={token}");
         }
         catch (BusinessRuleViolationException ex)
@@ -83,5 +66,4 @@ public class GoogleAuthController : ControllerBase
             return Redirect($"{clientUrl}/auth/error");
         }
     }
-
 }
